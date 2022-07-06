954abdb5fcd0df8cd4c2868a83486d28820473a7
Alexandre Tavares - F0436748 <alexandretavares@bb.com.br>
2022-07-06 00:54:36 -0300

    Add bootstrap

--- gamelibrary.py
+++ gamelibrary.py
@@ -1,6 +1,4 @@
-from re import template
-from tkinter.messagebox import YES
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 class Game:
 
@@ -9,15 +7,33 @@ class Game:
         self.category = category
         self.console = console
 
+game1 = Game("Tetris", "Puzzle", "Atari")
+game2 = Game("God of War", "Rack 'n slash", "PS2")
+game3 = Game("Mortal Kombat", "Fight", "PS2")
+
+game_list =[]
+game_list.append(game1)
+game_list.append(game2)
+game_list.append(game3)
+
 app = Flask(__name__)
 
-@app.route("/main")
-def hello_world():
-    game1 = Game("Tetris", "Puzzle", "Atari")
-    game2 = Game("God of War", "Rack 'n slash", "PS2")
-    game3 = Game("Mortal Kombat", "Fight", "PS2")
-    game_list = [game1, game2, game3]
+@app.route("/")
+def index():
     return render_template("list.html", title="Games", games=game_list)
+
+@app.route("/new")
+def new_game():
+    return render_template("newgame.html", title="New Game")
+
+@app.route("/add", methods=['POST',])
+def add():
+    name = request.form["name"]
+    category = request.form["category"]
+    console = request.form["console"]
+    game = Game(name, category, console)
+    game_list.append(game)
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
